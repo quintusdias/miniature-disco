7a526cad58bded55eaf6cccb402b06c4ed257bfe
John Evans <john.g.evans.ne@gmail.com>
2020-05-05 16:09:47 -0700

    Sync with harry

--- function_tests.py
+++ function_tests.py
@@ -14,11 +14,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has head about a cool new online to-do app.  She goes
+        # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        # She notices the page title and header mention do-do lists
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -30,12 +30,12 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        # She types "But peacock feathers" into a text box (Edith's hobby
-        # is typing fly-fishing lures)
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table.
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
@@ -45,20 +45,20 @@ class NewVisitorTest(unittest.TestCase):
             "New to-do item did not appear in table"
         )
 
-        # There is still a text box inviting her to add another item.  She
+        # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-        self.fail('Finish the test')
+        self.fail('Finish the test!')
 
-        # The page updates again, and now shows both items on her list.
+        # The page updates again, and now shows both items on her list
 
-        # Edith wonders whether the site will remember her list.  Then she sees
+        # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there.
 
-        # Satisfied, she goes back to sleep.
+        # Satisfied, she goes back to sleep
 
 
 if __name__ == '__main__':
